d9c24aabccb30a28626ba38951ab2d4f70ccece6
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-02-28 16:50:08 +0800

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // const validator = require('validator');
 const tourSchema = new mongoose.Schema(
@@ -86,6 +87,12 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationonWeeks').get(function(){
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
