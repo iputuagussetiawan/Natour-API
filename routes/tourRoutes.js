c97fc8f7a2c21623112b78cd4cc288ab26cbc803
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-02-28 15:40:34 +0800

    Aggregation Pipeline: Matching and Grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,8 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 //router.param('id', tourController.checkID);
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/top-5-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
