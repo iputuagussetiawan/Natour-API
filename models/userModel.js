d02a22bbbc7d95195a67daf0b7f06f2afeb41878
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-03 16:31:47 +0800

    Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -8,7 +11,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide your email'],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   photo: String,
   role: {
@@ -25,6 +29,13 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
@@ -36,7 +47,16 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
