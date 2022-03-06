295143aaad2ac6377e3394e683ec623f9b3cd2b8
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-06 15:20:10 +0800

    Building Handler Factory Functions: Delete

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
-
 const router = express.Router({ mergeParams: true });
 
 router.use(authController.protect);
@@ -13,6 +12,13 @@ router
     authController.protect,
     authController.restrictTo('user'),
     reviewController.createReview
+  );
+
+router
+  .route('/:id')
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
   );
 
 module.exports = router;
