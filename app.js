dc0864701572e9c1da0e57720bf187f144a7a9b6
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-18 18:19:54 +0800

    Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -75,6 +75,16 @@ app.get('/', (req, res) => {
     user: 'Agus'
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
