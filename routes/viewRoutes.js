36201482c85995797df0712f1db424f1c2868f53
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-20 16:53:35 +0800

    Building the User Account Page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour', authController.isLoggedIn, viewsController.getTour);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 //login
 router.get('/login', viewsController.getLoginForm);
